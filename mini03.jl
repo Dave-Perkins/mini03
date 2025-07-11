fbc8a9cec592e9c719a5fbb6edf6e3e2a8e50465
Dave Perkins <dperkins@colgate.edu>
2025-07-11 16:52:10 -0400

    Lots of cleanup

--- mini03.jl
+++ mini03.jl
@@ -89,9 +89,7 @@ function main(filename)
     # println("Running label propagation algorithm...")
     # label_propagation(g, node_info)
 
-    println("Running the updated algorithm three times...")
-    our_algorithm(g, edge_weights, node_info)
-    our_algorithm(g, edge_weights, node_info)
+    println("Running the updated algorithm...")
     our_algorithm(g, edge_weights, node_info)
 
     # Use a fixed-size color palette for cycling, e.g., 16 colors
